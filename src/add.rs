8890187dd1cc845abb9cdc2ef669757a41448ac7
thekuwayama <thekuwayama@gmail.com>
2021-01-19 09:41:37 +0900

    add

---
+++ src/add.rs
@@ -0,0 +1,19 @@
+use std::io::{Error, Write};
+
+pub fn add<W: Write>(writer: &mut W, s: &str) -> Result<(), Error> {
+    writer.write(format!("[ ] {} ()\n", s).as_bytes())?;
+    writer.flush()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut writer = Vec::new();
+        assert!(add(&mut writer, "test").is_ok());
+        assert_eq!(writer, b"[ ] test ()");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 #[macro_use]
 extern crate clap;
 
+mod add;
 mod list;
 
-use clap::{App, SubCommand};
+use clap::{App, Arg, SubCommand};
 use std::env;
-use std::fs::File;
-use std::io::BufReader;
+use std::fs::{File, OpenOptions};
+use std::io::{BufReader, BufWriter};
 
 fn main() {
     let app = App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
-        .subcommand(SubCommand::with_name("list"));
+        .subcommand(SubCommand::with_name("list"))
+        .subcommand(SubCommand::with_name("add").arg(Arg::with_name("task")));
     let matches = app.get_matches();
 
-    let f = File::open("/Users/thekuwayama/.todo").unwrap(); // TODO: home dir
-    let mut reader = BufReader::new(f);
-
-    let result = match matches.subcommand() {
-        ("list", _) => list::list(&mut reader).map_or_else(|e| e.to_string(), |v| v),
-        _ => String::from("help"), // TODO: print help
+    match matches.subcommand() {
+        ("list", _) => {
+            let f = File::open("/Users/thekuwayama/.todo").unwrap(); // TODO: home dir
+            let mut reader = BufReader::new(f);
+            let res = list::list(&mut reader).unwrap_or(String::from("list error"));
+            println!("{}", res);
+            ()
+        }
+        ("add", Some(s)) => {
+            let f = OpenOptions::new()
+                .append(true)
+                .open("/Users/thekuwayama/.todo")
+                .unwrap();
+            let mut writer = BufWriter::new(f);
+            add::add(&mut writer, s.value_of("task").unwrap()).unwrap();
+            ()
+        }
+        ("add", None) => (),
+        _ => (), // TODO: print help
     };
-    println!("{}", result);
 }
