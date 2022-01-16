1761e8a7b8acb212a950416bedb97ec3b79261a0
thekuwayama <thekuwayama@gmail.com>
2022-01-16 20:58:48 +0900

    feat: gen todo-completion.bash using clap_complete

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::error;
+
+use clap_complete::{generate_to, shells};
+
+include!("src/cli.rs");
+
+const COMPLETION_FILENAME: &str = "todo-completion";
+
+fn main() -> Result<(), Box<dyn error::Error + Send + Sync + 'static>> {
+    let mut app = build_cli();
+    let _ = generate_to(shells::Bash, &mut app, COMPLETION_FILENAME, ".")?;
+
+    Ok(())
+}
